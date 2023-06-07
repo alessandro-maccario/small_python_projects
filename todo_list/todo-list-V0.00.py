3de0a5d894422ab3dc7f57acaf82465be79cf0a6
alessandro-maccario <a.maccario1@campus.unimib.it>
2023-06-07 07:51:14 +0200

    Added necessary packages to env

---
+++ todo_list/todo-list-V0.00.py
@@ -0,0 +1,11 @@
+"""
+    Small python project on creating a Simple Todo app in order to learn more about
+    Software Engineer.
+"""
+
+# IMPORT PACKAGES
+import numpy as np
+import pandas as pd
+
+
+
