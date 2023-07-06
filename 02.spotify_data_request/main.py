a844ae01a2915d533a8930ac5837c8d3da747927
alessandro-maccario <a.maccario1@campus.unimib.it>
2023-07-06 20:02:44 +0200

    Creating main script for Spotify API request

---
+++ 02.spotify_data_request/main.py
@@ -0,0 +1,15 @@
+"""
+    Following this tutorial:
+        - https://towardsdatascience.com/extracting-song-data-from-the-spotify-api-using-python-b1e79388d50
+
+    To fetch some data from the Spotify API.
+    Open the website in Incognito mode to read it as many times as you want.
+
+    Here the steps:
+    - Part I: (This article)
+    - Part II: EDA and Clustering
+    - Part III: Building a Song Recommendation System with Spotify
+    - Part IV: Deploying a Spotify Recommendation Model with Flask
+
+    This file takes into account Part I.
+"""
